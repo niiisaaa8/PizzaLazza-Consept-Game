158006ab21bafa71e0a6a859849680485c689fd7
niiisaaa8 <havva.isik@ogr.bandirma.edu.tr>
2025-12-23 13:33:18 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace burger_avcisi_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.oyunTimer = new System.Windows.Forms.Timer(this.components);
+            this.pnlGiris = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.pizzakutusu = new System.Windows.Forms.PictureBox();
+            this.pnlGiris.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pizzakutusu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.BackColor = System.Drawing.Color.Transparent;
+            this.lblSkor.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSkor.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblSkor.Location = new System.Drawing.Point(12, 18);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(142, 45);
+            this.lblSkor.TabIndex = 1;
+            this.lblSkor.Text = "Skor: 0";
+            // 
+            // oyunTimer
+            // 
+            this.oyunTimer.Interval = 17;
+            // 
+            // pnlGiris
+            // 
+            this.pnlGiris.BackColor = System.Drawing.Color.Transparent;
+            this.pnlGiris.BackgroundImage = global::burger_avcisi_app.Properties.Resources.mutfakResim;
+            this.pnlGiris.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlGiris.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlGiris.Controls.Add(this.pictureBox1);
+            this.pnlGiris.Controls.Add(this.btnBasla);
+            this.pnlGiris.Controls.Add(this.label1);
+            this.pnlGiris.Controls.Add(this.lblSure);
+            this.pnlGiris.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGiris.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.pnlGiris.Location = new System.Drawing.Point(0, 0);
+            this.pnlGiris.Name = "pnlGiris";
+            this.pnlGiris.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.pnlGiris.Size = new System.Drawing.Size(1545, 755);
+            this.pnlGiris.TabIndex = 3;
+            this.pnlGiris.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlGiris_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(18, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.BackColor = System.Drawing.Color.Crimson;
+            this.btnBasla.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBasla.FlatAppearance.BorderSize = 0;
+            this.btnBasla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBasla.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.ForeColor = System.Drawing.Color.White;
+            this.btnBasla.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnBasla.Location = new System.Drawing.Point(434, 418);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(338, 98);
+            this.btnBasla.TabIndex = 1;
+            this.btnBasla.Text = "OYUNA BAŞLA";
+            this.btnBasla.UseVisualStyleBackColor = false;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(296, 306);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(596, 84);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LEZZET YAĞMURU";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoSize = true;
+            this.lblSure.BackColor = System.Drawing.Color.Transparent;
+            this.lblSure.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSure.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblSure.Location = new System.Drawing.Point(1124, 16);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(162, 45);
+            this.lblSure.TabIndex = 2;
+            this.lblSure.Text = "Süre: 10";
+            // 
+            // pizzakutusu
+            // 
+            this.pizzakutusu.BackColor = System.Drawing.Color.Transparent;
+            this.pizzakutusu.Image = global::burger_avcisi_app.Properties.Resources.pizzakutusu;
+            this.pizzakutusu.Location = new System.Drawing.Point(485, 580);
+            this.pizzakutusu.Name = "pizzakutusu";
+            this.pizzakutusu.Size = new System.Drawing.Size(271, 201);
+            this.pizzakutusu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pizzakutusu.TabIndex = 3;
+            this.pizzakutusu.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1545, 755);
+            this.Controls.Add(this.pnlGiris);
+            this.Controls.Add(this.pizzakutusu);
+            this.Controls.Add(this.lblSkor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.pnlGiris.ResumeLayout(false);
+            this.pnlGiris.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pizzakutusu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Timer oyunTimer;
+        private System.Windows.Forms.Panel pnlGiris;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.PictureBox pizzakutusu;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace burger_avcisi_app
+{
+    public partial class Form1 : Form
+    {
+        // --- DEĞİŞKENLER ---
+        int skor = 0;
+        int kalanSure = 10;
+        int saniyeSayaci = 0;
+        Random rastgele = new Random();
+        List<PictureBox> dusenNesneler = new List<PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "PizzaLazza - Lezzet Avcısı";
+
+            // --- İŞTE BU EKSİK KABLOLAR (Bunları Ekle) ---
+            oyunTimer.Tick += new EventHandler(OyunZamani); // Motoru bağla
+            this.KeyDown += new KeyEventHandler(TuşaBasildi); // Klavyeyi bağla
+                                   
+
+            // --- 1. BAŞLANGIÇ TEMİZLİĞİ ---
+            this.DoubleBuffered = true; // Donmayı engeller
+
+            // Oyunu durdur (Otomatik başlamasın)
+            oyunTimer.Enabled = false;
+
+            oyunTimer.Stop();
+            // Menüyü ortala
+            Form1_Resize(null, null);
+        }
+
+        // --- 2. OYUNA BAŞLA BUTONU ---
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+
+            // İŞTE BU SATIRI EKLE:
+            lblSure.BringToFront();
+        
+           // 1. Yazıyı formun kendisine bağla (Yanlışlıkla başka panelin içine kaçmasın)
+           lblSure.Parent = this;
+
+            // 2. Yazıyı ekranın görünen yerine ışınla (X=600, Y=20)
+            lblSure.Location = new Point(1250, 20);
+
+            // 3. Yazıyı en öne getir (Resimlerin altında kalmasın)
+            lblSure.BringToFront();
+
+            // 4. Rengini CIRTLAK KIRMIZI yap ki kesin görelim
+            lblSure.ForeColor = Color.DarkRed;
+
+            // 5. Görünür olduğundan emin ol
+            lblSure.Visible = true;
+            // Giriş ekranını kapat
+            pnlGiris.Visible = false;
+
+            // Ekranın genişliğinden sepeti çıkar, ikiye böl = Tam Orta
+            pizzakutusu.Left = (this.ClientSize.Width - pizzakutusu.Width) / 2;
+          
+            // Garsonu sahneye çağır ve EN ÖNE al
+            pizzakutusu.Visible = true;
+            pizzakutusu.BringToFront();
+
+            // Oyunu başlat
+            oyunTimer.Start();
+
+            // Odağı forma ver (Klavye çalışsın diye)
+            this.Focus();
+        }
+
+        // --- 3. EKRAN BOYUTU DEĞİŞİRSE ---
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // Menüdeki Başlığı Ortala (label1 senin başlığınsa ismini kontrol et)
+            // Eğer başlığının adı lblBaslik ise burayı değiştir
+            try
+            {
+                // Menü başlığını ortala
+                foreach (Control c in pnlGiris.Controls)
+                {
+                    if (c is Label) // Başlık
+                    {
+                        c.Left = (this.ClientSize.Width - c.Width) / 2;
+                        c.Top = (this.ClientSize.Height / 2) - 80;
+                    }
+                    if (c is Button) // Buton
+                    {
+                        c.Left = (this.ClientSize.Width - c.Width) / 2;
+                        c.Top = (this.ClientSize.Height / 2) + 20;
+                    }
+                }
+            }
+            catch { }
+
+            // Sepeti aşağı sabitle
+            if (pizzakutusu != null)
+                pizzakutusu.Top = this.ClientSize.Height - pizzakutusu.Height;
+        }
+
+        // --- 4. KLAVYE KONTROLÜ ---
+        private void TuşaBasildi(object sender, KeyEventArgs e)
+        {
+            int sepetHizi = 45; // Hız
+            if (e.KeyCode == Keys.Left && pizzakutusu.Left > 0)
+                pizzakutusu.Left -= sepetHizi;
+            else if (e.KeyCode == Keys.Right && pizzakutusu.Right < this.ClientSize.Width)
+                pizzakutusu.Left += sepetHizi;
+        }
+
+        // --- 5. OYUN ZAMANI (HER ŞEY BURADA DÖNÜYOR) ---
+        // --- OYUN ZAMANI (HER ŞEYİN OLDUĞU ANA MOTOR) ---
+        private void OyunZamani(object sender, EventArgs e)
+        {
+            // 1. SEPETİ AŞAĞIYA ÇAK (Ekran boyutu değişse bile)
+            if (pizzakutusu != null)
+                pizzakutusu.Top = this.ClientSize.Height - pizzakutusu.Height;
+
+
+            // 3. SÜRE SAYACI
+            saniyeSayaci++;
+            if (saniyeSayaci >= 40) // Yaklaşık 1 saniye geçince
+            {
+                saniyeSayaci = 0;
+                kalanSure--;
+                try { lblSure.Text = "Süre: " + kalanSure; } catch { }
+
+                // SÜRE BİTTİ Mİ? (KAYBETME)
+                if (kalanSure <= 0)
+                {
+                    oyunTimer.Stop();
+                    // Windows'un Hata sesini çal (Bitiş uyarısı)
+                    System.Media.SoundPlayer ses = new System.Media.SoundPlayer(Properties.Resources.kaybedis_wav);
+                    ses.Play();
+
+                    DialogResult cevap = MessageBox.Show(
+                        "SÜRE BİTTİ! Topladığın Puan: " + skor + "\n\nTekrar denemek ister misin?",
+                        "Oyun Sonu",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question);
+
+                    if (cevap == DialogResult.Yes) OyunuSifirla();
+                    else Application.Exit();
+                }
+            }
+
+            // 4. YENİ NESNE ÜRETME (%6 Şansla)
+            if (rastgele.Next(0, 100) < 6)
+            {
+                NesneUret();
+            }
+
+            // 5. HAREKET VE ÇARPIŞMA KONTROLÜ
+            for (int i = dusenNesneler.Count - 1; i >= 0; i--)
+            {
+                PictureBox nesne = dusenNesneler[i];
+                nesne.Top += 7; // Düşme hızı
+
+                // A) Yere düştü mü? (Ekrandan çıktı mı?)
+                if (nesne.Top > this.ClientSize.Height)
+                {
+                    this.Controls.Remove(nesne);
+                    dusenNesneler.Remove(nesne);
+                }
+                // B) Sepete çarptı mı? (Yakaladık!)
+                else if (nesne.Bounds.IntersectsWith(pizzakutusu.Bounds))
+                {
+                    string tur = nesne.Tag.ToString();
+
+                    if (tur == "Pizza")
+                    {
+                        skor += 20;
+                        // OLUMLU SES (Dın!)
+                        System.Media.SoundPlayer ses = new System.Media.SoundPlayer(Properties.Resources.ye_wav);
+                        ses.Play();
+                    }
+                    else // Brokoli
+                    {
+                        skor -= 10;
+                        // OLUMSUZ SES (Düm!)
+                        System.Media.SoundPlayer ses = new System.Media.SoundPlayer(Properties.Resources.hata_wav);
+                        ses.Play();
+                    }
+
+                    // Skoru güncelle ve nesneyi yok et
+                    lblSkor.Text = "Skor: " + skor;
+                    this.Controls.Remove(nesne);
+                    dusenNesneler.Remove(nesne);
+                }
+            }
+
+            // 6. KAZANMA KONTROLÜ (Hedef 100 Puan Olsun)
+            if (skor >= 100)
+            {
+                oyunTimer.Stop();
+                System.Media.SoundPlayer ses = new System.Media.SoundPlayer(Properties.Resources.kazanis_wav);
+                ses.Play();
+
+                DialogResult cevap = MessageBox.Show(
+                    "TEBRİKLER ŞAMPİYON! \n PİZZALAZZA'dan %20 İndirim Kodun: LAZZA20\n\nTekrar oynamak ister misin?",
+                    "Zafer!",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Asterisk);
+
+                if (cevap == DialogResult.Yes) OyunuSifirla();
+                else Application.Exit();
+            }
+        }
+
+        // --- 6. NESNE ÜRETME ---
+        private void NesneUret()
+        {
+            PictureBox yeniNesne = new PictureBox();
+            yeniNesne.Size = new Size(110, 110);
+            yeniNesne.Top =-100;
+            yeniNesne.Left = rastgele.Next(20, this.ClientSize.Width - 100);
+            yeniNesne.SizeMode = PictureBoxSizeMode.StretchImage;
+            yeniNesne.BackColor = Color.Transparent;
+
+            int sans = rastgele.Next(0, 100);
+           
+           if (sans < 60) // pizza
+            {
+                yeniNesne.Tag = "Pizza";
+                try { yeniNesne.Image = Properties.Resources.pizzaResim; } catch { yeniNesne.BackColor = Color.Orange; }
+            }
+            else // çürük
+            {
+                yeniNesne.Tag = "Yanık Pizza";
+                try { yeniNesne.Image = Properties.Resources.yanıkPizza; } catch { yeniNesne.BackColor = Color.Green; }
+            }
+
+            dusenNesneler.Add(yeniNesne);
+            this.Controls.Add(yeniNesne);
+            yeniNesne.BringToFront();
+
+            // Eğer menü açıksa nesneyi arkaya at (Hata önleyici)
+            if (pnlGiris.Visible == true) yeniNesne.SendToBack();
+        }
+
+        // --- 7. OYUNU SIFIRLA ---
+        private void OyunuSifirla()
+        {
+            skor = 0;
+            kalanSure = 15;
+            lblSkor.Text = "Skor: 0";
+            lblSure.Text = "Süre: 15";
+
+            foreach (PictureBox nesne in dusenNesneler) this.Controls.Remove(nesne);
+            dusenNesneler.Clear();
+
+            // Giriş ekranına dön
+            pnlGiris.Visible = true;
+            pizzakutusu.Visible = false;
+            // Ekranın genişliğinden sepeti çıkar, ikiye böl = Tam Orta
+            pizzakutusu.Left = (this.ClientSize.Width - pizzakutusu.Width) / 2;
+            oyunTimer.Stop(); // Zamanı durdur, butona basılmasını bekle
+          
+      
+        }
+
+        // Hata Önleyiciler (Boş kalsın)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Sepet (Kutu) ayarı buraya gelecek
+            pizzakutusu.Size = new Size(150, 100);
+            pizzakutusu.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlGiris_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
